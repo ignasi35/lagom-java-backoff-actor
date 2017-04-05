801a06165f656604197f6e7fc7b12adedef2614e
Tim Moore <tim.moore@lightbend.com>
2017-04-05 16:13:00 +1000

    Complete the CRUD actor

--- hello-impl/src/main/java/com/example/hello/impl/HelloServiceImpl.java
+++ hello-impl/src/main/java/com/example/hello/impl/HelloServiceImpl.java
@@ -7,74 +7,85 @@ import akka.Done;
 import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.PoisonPill;
 import akka.actor.Props;
 import akka.cluster.singleton.ClusterSingletonManager;
 import akka.cluster.singleton.ClusterSingletonManagerSettings;
+import akka.cluster.singleton.ClusterSingletonProxy;
+import akka.cluster.singleton.ClusterSingletonProxySettings;
 import akka.pattern.Backoff;
 import akka.pattern.BackoffSupervisor;
-import akka.pattern.Patterns;
 import akka.util.Timeout;
 import com.example.hello.api.GreetingMessage;
 import com.example.hello.api.HelloService;
 import com.example.hello.impl.HelloCommand.Hello;
 import com.example.hello.impl.HelloCommand.UseGreetingMessage;
 import com.lightbend.lagom.javadsl.api.ServiceCall;
-import com.lightbend.lagom.javadsl.persistence.PersistentEntityRef;
-import com.lightbend.lagom.javadsl.persistence.PersistentEntityRegistry;
 import com.lightbend.lagom.javadsl.persistence.cassandra.CassandraSession;
 import scala.concurrent.duration.Duration;
+import scala.concurrent.duration.FiniteDuration;
 
 import javax.inject.Inject;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
+
+import static akka.pattern.PatternsCS.ask;
 
 /**
  * Implementation of the HelloService.
  */
 public class HelloServiceImpl implements HelloService {
 
-    private final PersistentEntityRegistry persistentEntityRegistry;
+    private static final Timeout DEFAULT_TIMEOUT = new Timeout(new FiniteDuration(10, TimeUnit.SECONDS));
+
+    private final ActorRef singletonProxy;
 
     @Inject
-    public HelloServiceImpl(PersistentEntityRegistry persistentEntityRegistry, ActorSystem system, CassandraSession cassandraSession) {
-        this.persistentEntityRegistry = persistentEntityRegistry;
-        persistentEntityRegistry.register(HelloEntity.class);
-
+    public HelloServiceImpl(ActorSystem system, CassandraSession cassandraSession) {
         Props yourRealActorProps = PrepareDBActor.props(cassandraSession);
 
-        Props targetActorProps = BackoffSupervisor.props(
-                Backoff.onStop(
+        Props backoffProps = BackoffSupervisor.props(
+                Backoff.onFailure(
                         yourRealActorProps,
                         "your-real-actor",
                         Duration.create(3, TimeUnit.SECONDS),
                         Duration.create(30, TimeUnit.SECONDS),
-                        0.2));
+                        0.2
+                )
+        );
 
+        // Start the cluster singleton
         ClusterSingletonManagerSettings settings = ClusterSingletonManagerSettings.create(system);
-        ActorRef ref = system.actorOf(ClusterSingletonManager.props(targetActorProps, akka.actor.PoisonPill.getInstance(), settings), "my-supervisor");
+        ActorRef singleton = system.actorOf(
+                ClusterSingletonManager.props(backoffProps, PoisonPill.getInstance(), settings),
+                "my-singleton"
+        );
 
-        Patterns.ask(ref, new PrepareDBActor.Execute(), Timeout.apply(10, TimeUnit.SECONDS));
-
+        ClusterSingletonProxySettings proxySettings = ClusterSingletonProxySettings.create(system);
+        singletonProxy = system.actorOf(
+                ClusterSingletonProxy.props(
+                        singleton.path().toStringWithoutAddress(),
+                        proxySettings
+                ),
+                "my-singleton-proxy"
+        );
     }
 
     @Override
     public ServiceCall<NotUsed, String> hello(String id) {
         return request -> {
-            // Look up the hello world entity for the given ID.
-            PersistentEntityRef<HelloCommand> ref = persistentEntityRegistry.refFor(HelloEntity.class, id);
-            // Ask the entity the Hello command.
-            return ref.ask(new Hello(id, Optional.empty()));
-            //return CompletableFuture.completedFuture("Hello someone");
+            // Ask the singleton actor the Hello command.
+            return ask(singletonProxy, new Hello(id, Optional.empty()), DEFAULT_TIMEOUT)
+                    .thenApply(result -> (String) result);
         };
     }
 
     @Override
     public ServiceCall<GreetingMessage, Done> useGreeting(String id) {
         return request -> {
-            // Look up the hello world entity for the given ID.
-            PersistentEntityRef<HelloCommand> ref = persistentEntityRegistry.refFor(HelloEntity.class, id);
-            // Tell the entity to use the greeting message specified.
-            return ref.ask(new UseGreetingMessage(request.message));
+            // Tell the singleton actor to use the greeting message specified.
+            return ask(singletonProxy, new UseGreetingMessage(id, request.message), DEFAULT_TIMEOUT)
+                    .thenApply(result -> Done.getInstance());
         };
 
     }
